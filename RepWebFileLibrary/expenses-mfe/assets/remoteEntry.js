b867689aeae8026788ba9dcbf5cd9569f59971cc
Ruslan Lavreniuk <ruslan.lavreniuk@kore.solutions>
2025-09-01 21:24:21 -0400

    feat(expenses): Dialog window 2

--- RepWebFileLibrary/expenses-mfe/assets/remoteEntry.js
+++ RepWebFileLibrary/expenses-mfe/assets/remoteEntry.js
@@ -2,7 +2,7 @@ const currentImports = {};
 const exportSet = /* @__PURE__ */ new Set(["Module", "__esModule", "default", "_export_sfc"]);
 let moduleMap = {
   "./mount": () => {
-    dynamicLoadingCss(["style-CiEPX7YZ.css"], false, "./mount");
+    dynamicLoadingCss(["style-jYrDYLbA.css"], false, "./mount");
     return __federation_import("./assets/__federation_expose_Mount-C5NYmzZl.js").then((module) => Object.keys(module).every((item) => exportSet.has(item)) ? () => module.default : () => module);
   }
 };
