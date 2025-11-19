4540a5708435039018a8c119c5e139065cb57416
Ruslan Lavreniuk <ruslan.lavreniuk@kore.solutions>
2025-11-19 15:48:39 -0500

    feat(expensesApp): #TRX-166 Mileage by period form

---
+++ RepWebFileLibrary/expenses-mfe-test/assets/ExpensesPage-BV40IeaJ.js
@@ -0,0 +1,8 @@
+import { j as jsxRuntimeExports } from "./jsx-runtime-CzdF90_e.js";
+import { E as ExpensesList } from "./ExpensesList-Ny2FmLOX.js";
+const ExpensesPage = () => {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(ExpensesList, {});
+};
+export {
+  ExpensesPage as default
+};
